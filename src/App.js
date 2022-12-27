430bb195ce43f3729d16640aec05e48143514464
miheer <sarangtamkhane10@gmail.com>
2022-12-27 14:09:07 +0530

    project setup

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import { Button } from "react-bootstrap";
+function App() {
+  return (
+    <div className="App">
+      <h1>NBA TEAMS</h1>
+      <Button>Click me</Button>
+    </div>
+  );
+}
+
+export default App;
