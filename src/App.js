1718343de375f3512b605d70cd3d5e2b9b84f2da
miheer <sarangtamkhane10@gmail.com>
2022-12-28 15:55:08 +0530

    fix build problem

--- src/App.js
+++ src/App.js
@@ -1,9 +1,6 @@
-import { Button } from "react-bootstrap";
 import "./App.css";
 import { TeamsTable, OffCanvas, Search } from "./components";
-import { useTeams } from "./context/TeamsContext";
 function App() {
-  const { dispatchData } = useTeams();
   return (
     <div className="App">
       <h1 className="heading">NBA TEAMS</h1>
