75e5a754abcb6e039a5c2b9510ab514da75315a7
miheer <sarangtamkhane10@gmail.com>
2023-01-25 11:19:18 +0530

    delete the debouce function file

--- src/utils/debouce.js
+++
@@ -1,9 +0,0 @@
-export function debounce(fn, delay = 300) {
-  let timeout;
-  return function (...args) {
-    if (timeout) clearTimeout(timeout);
-    timeout = setTimeout(() => {
-      return fn.apply(this, args);
-    }, delay);
-  };
-}
